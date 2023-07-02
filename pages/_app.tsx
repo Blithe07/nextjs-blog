bbbd38a3ecb91238dffd7b4f468c40ef77102603
Blithe07 <916604923@qq.com>
2023-07-03 00:24:38 +0800

    Optimize antd style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+import Loading from "@/components/loading"
 import type { AppProps } from "next/app"
+import dynamic from "next/dynamic"
 import "../styles/global.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const DynamicComponentWithNoSSR = dynamic(() => import("./index"), {
+    loading: () => <Loading />,
+    ssr: false, // 禁用服务器端渲染
+  })
+  return <DynamicComponentWithNoSSR {...pageProps} />
 }
