ec4565ddb49d88b1ff6fabadc5b0a692cb4bb0d4
SZ\laitongbin <laitongbin@rayvision.com>
2023-07-04 10:03:26 +0800

    Fix _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import type { AppProps } from "next/app"
 import dynamic from "next/dynamic"
 import "../styles/global.css"
 
-export default function App({ Component, pageProps }: AppProps) {
-  const url = Component.name === 'Home' ? 'index' : 'posts/[id]'
+export default function App({ pageProps }: AppProps) {
+  const url = pageProps.allPostsData ? 'index' : 'posts/[id]'
   const DynamicComponentWithNoSSR = dynamic(() => import(`./${url}`), {
     loading: () => <Loading />,
     ssr: false, // 禁用服务器端渲染
